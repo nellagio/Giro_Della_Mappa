2455ffe7e769e08043ed13f88971cba5fa030da1
nellagio <nickgallo41@gmail.com>
2020-06-19 13:08:02 -0700

    models complete

--- mygiro/girodm/models.py
+++ mygiro/girodm/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Pace(models.Model):
+    name = models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return self.name
+        
+class Ride(models.Model):
+
+    host_name = models.CharField(max_length=30)
+    start_location = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    waypoint = models.CharField(max_length=200)
+    end_location = models.CharField(max_length=200)
+    end_time = models.DateTimeField()
+    private = models.BooleanField(default=False)
+    comments = models.CharField(max_length=400)
+    pace = models.ManyToManyField(Pace)
+
+    def __str__(self):
+        return self.name
+    
