9f888530a6eb23e80b58b0d15bfc105b271d8aa7
nellagio <nickgallo41@gmail.com>
2020-07-15 02:54:37 -0700

    it was a late one, a lot of styling and spelling errors fixed

--- mygiro/girodm/static/girodm/js/hostaride.js
+++ mygiro/girodm/static/girodm/js/hostaride.js
@@ -10,39 +10,39 @@ window.initMap = function () {
         let user_longitude = position.coords.longitude
         let user_coords = { lat: user_latitude, lng: user_longitude }
         console.log(user_coords)
-        map = new google.maps.Map(document.getElementById('map'), {
+        startMap = new google.maps.Map(document.getElementById('startMap'), {
             center: user_coords,
             zoom: 12
-        })
-        let bikeLayer = new google.maps.BicyclingLayer()
-        bikeLayer.setMap(map)
+        });
+        let bikeLayer = new google.maps.BicyclingLayer();
+        bikeLayer.setMap(startMap);
         let user_coords_marker = new google.maps.Marker({
             position: user_coords,
-            map: map,
+            map: startMap,
             title: 'Your location!'
-        })
+        });
         // start coords
-        let start_input = document.getElementById('autocompleteStart')
-        let start_autocomplete = new google.maps.places.Autocomplete(start_input)
-        start_autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+        let start_input = document.getElementById('autocompleteStart');
+        let start_autocomplete = new google.maps.places.Autocomplete(start_input);
+        start_autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
         start_autocomplete.addListener('place_changed', function () {
-            let start_place = start_autocomplete.getPlace()
+            let start_place = start_autocomplete.getPlace();
             if (!start_place.geometry) {
                 // User entered the name of a start_place that was not suggested and
                 // pressed the Enter key, or the start_place Details request failed.
-                window.alert("No details available for input: '" + start_place.name + "'")
-                return
+                window.alert("No details available for input: '" + start_place.name + "'");
+                return;
             }
 
             // If the start_place has a geometry, then present it on a map.
             if (start_place.geometry.viewport) {
-                map.fitBounds(start_place.geometry.viewport)
+                startMap.fitBounds(start_place.geometry.viewport);
             } else {
-                map.setCenter(start_place.geometry.location)
-                map.setZoom(17)  // Why 17? Because it looks good.
+                startMap.setCenter(start_place.geometry.location);
+                startMap.setZoom(17);  // Why 17? Because it looks good.
             }
 
-            let address = ''
+            let address = '';
             if (start_place.address_components) {
                 address = [
                     (start_place.address_components[0] && start_place.address_components[0].short_name || ''),
@@ -50,13 +50,6 @@ window.initMap = function () {
                     (start_place.address_components[2] && start_place.address_components[2].short_name || '')
                 ].join(' ')
                 
-                let start_coords_marker = new google.maps.Marker({
-                    position: start_coords,
-                    draggable: true,
-                    animation: google.maps.Animation.DROP,
-                    map: map,
-                    title: 'Where a ride starts',
-                })
                 let start_lat = start_place.geometry.location.lat()
                 let start_lng = start_place.geometry.location.lng()
                 autoCompleteLatStart.value = start_lat
@@ -64,33 +57,44 @@ window.initMap = function () {
                 let start_coords = { lat: start_lat, lng: start_lng }
                 console.log(start_coords)
                 console.log("start")
+
+                let start_coords_marker = new google.maps.Marker({
+                    position: start_coords,
+                    draggable: true,
+                    animation: google.maps.Animation.DROP,
+                    map: startMap,
+                    title: 'Where a ride starts',
+                });
             }
         })
 
         // end coords
-
-        let end_input = document.getElementById('autocompleteEnd')
-        let end_autocomplete = new google.maps.places.Autocomplete(end_input)
-        end_autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+        endMap = new google.maps.Map(document.getElementById('endMap'), {
+            center: user_coords,
+            zoom: 12
+        });
+        let end_input = document.getElementById('autocompleteEnd');
+        let end_autocomplete = new google.maps.places.Autocomplete(end_input);
+        end_autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
 
         end_autocomplete.addListener('place_changed', function () {
-            let end_place = end_autocomplete.getPlace()
+            let end_place = end_autocomplete.getPlace();
             if (!end_place.geometry) {
                 // User entered the name of a end_place that was not suggested and
                 // pressed the Enter key, or the end_place Details request failed.
-                window.alert("No details available for input: '" + end_place.name + "'")
-                return
+                window.alert("No details available for input: '" + end_place.name + "'");
+                return;
             }
 
             // If the end_place has a geometry, then present it on a map.
             if (end_place.geometry.viewport) {
-                map.fitBounds(end_place.geometry.viewport)
+                endMap.fitBounds(end_place.geometry.viewport);
             } else {
-                map.setCenter(end_place.geometry.location)
-                map.setZoom(17)  // Why 17? Because it looks good.
+                endMap.setCenter(end_place.geometry.location);
+                endMap.setZoom(17);  // Why 17? Because it looks good.
             }
 
-            let address = ''
+            let address = '';
             if (end_place.address_components) {
                 address = [
                     (end_place.address_components[0] && end_place.address_components[0].short_name || ''),
@@ -109,14 +113,14 @@ window.initMap = function () {
                     position: end_coords,
                     draggable: true,
                     animation: google.maps.Animation.DROP,
-                    map: map,
+                    map: endMap,
                     title: 'Where the ride ends',
-                })
+                });
             }
         })
 
     })
 }
 $(document).on("keydown", "form", function(event) { 
-    return event.key != "Enter"
-})
+    return event.key != "Enter";
+});
