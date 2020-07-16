bb82b56822f9d560c12a454e82de980335097681
nellagio <nickgallo41@gmail.com>
2020-07-16 14:34:42 -0700

    fixing an error

--- girodm/views.py
+++ girodm/views.py
@@ -12,18 +12,6 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-def handler404(request, exception, template_name="404.html"):
-    response = render_to_response(template_name)
-    response.status_code = 404
-    return response
-
-
-def handler500(request, exception, template_name="500.html"):
-    response = render_to_response(template_name)
-    response.status_code = 500
-    return response
 
 def index(request):
     rides = Ride.objects.filter(private=False)
